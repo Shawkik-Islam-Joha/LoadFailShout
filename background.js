da5ebe82c7b4c971ae1fa88e338b32b21932bf84
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2026-02-24 22:04:57 +0600

    Implemented New Architechture

--- background.js
+++ background.js
@@ -1,22 +1,11 @@
-let lastPlayed = 0;
-
 chrome.webNavigation.onErrorOccurred.addListener((details) => {
 
-    // Only trigger for main frame (not iframes)
+    // Only main page
     if (details.frameId !== 0) return;
 
-    // Prevent spam (2 second cooldown)
-    const now = Date.now();
-    if (now - lastPlayed < 2000) return;
-    lastPlayed = now;
-
-    chrome.scripting.executeScript({
-        target: { tabId: details.tabId },
-        func: playSound
+    // Open our own fail page
+    chrome.tabs.update(details.tabId, {
+        url: chrome.runtime.getURL("fail.html")
     });
-});
 
-function playSound() {
-    const audio = new Audio(chrome.runtime.getURL("sound.mp3"));
-    audio.play();
-}
+});
---
+++ fail.js
@@ -0,0 +1,2 @@
+const audio = new Audio(chrome.runtime.getURL("sound.mp3"));
+audio.play();
